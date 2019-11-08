4aae846ac7a2d1899449b9eef4dfdde6623f8693
Bryan Chem <chem.bryan@gmail.com>
2019-11-08 13:23:17 -0500

    Create initial file structure
    Add initial partitioning of functions

---
+++ main.m
@@ -0,0 +1,24 @@
+%  General 1D FE solver
+%  Author: Bryan Chem
+
+%-------------------------   Parameters   --------------------------------%
+
+%-------------------------------------------------------------------------%
+
+%  Initialize data
+
+%  Assemble global stiffness matrix
+
+%  Element wise stiffness
+
+%  Add to the global stiffness matrix
+
+%  Applying the boundary condition
+
+%  Assemble global mass matrix
+
+%  Multiply by the density
+    
+%  Application of boundary condition removes first column and row
+
+%  Solve system
